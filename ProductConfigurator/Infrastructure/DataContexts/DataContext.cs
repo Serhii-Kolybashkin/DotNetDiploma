691f360a9b1d560450c42c62ff5e276505a34ec1
Serhii-Kolybashkin <104523821+Serhii-Kolybashkin@users.noreply.github.com>
2022-08-09 23:21:23 +0300

    Diplima_Template

---
+++ ProductConfigurator/Infrastructure/DataContexts/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataContexts
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Component> components { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<ProductAssembled> productAssembledes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(nameof(DataContext));
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ProductConfigurator/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.DataContexts;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository
+    {
+        private DataContext _dataContext;
+
+        public OrderRepository(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+    }
+}
